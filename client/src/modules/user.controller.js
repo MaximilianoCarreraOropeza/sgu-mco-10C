d2e91a1cf9757c16a9cdf745808a16e167c518fe
unknown <20223tn012@utez.edu.mx>
2025-11-17 20:17:07 -0600

    fourth commit

--- client/src/modules/user.controller.js
+++ client/src/modules/user.controller.js
@@ -2,7 +2,7 @@ const UserController = {}
 
 const ENV = import.meta.env
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`
+const API_URL = `${ENV.VITE_API_PROTOCOL}://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`
 
 UserController.getUsers = async () => {
     try {
